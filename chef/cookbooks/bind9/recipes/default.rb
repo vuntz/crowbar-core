b8dd62b5ce154837551feda82304a4880f441859
Jiří Suchomel <jsuchome@suse.cz>
2015-10-01 09:01:29 +0200

    [bind] replace check for platform with check for platform_family

--- chef/cookbooks/bind9/recipes/default.rb
+++ chef/cookbooks/bind9/recipes/default.rb
@@ -17,25 +17,25 @@ include_recipe "utils"
 require "ipaddr"
 
 package "bind9" do
-  case node[:platform]
-  when "centos","redhat", "suse"
+  case node[:platform_family]
+  when "rhel", "suse"
     package_name "bind"
   end
   action :install
 end
 package "bind9utils" do
-  case node[:platform]
-  when "centos","redhat", "suse"
+  case node[:platform_family]
+  when "rhel", "suse"
     package_name "bind-utils"
   end
   action :install
 end
 
-binduser, bindgroup = case node[:platform]
-  when "ubuntu","debian"
-    ["bind", "bind"]
-  when "centos","redhat","suse"
-    ["named", "named"]
+case node[:platform_family]
+when "debian"
+  binduser, bindgroup = ["bind", "bind"]
+when "rhel", "suse"
+  binduser, bindgroup = ["named", "named"]
 end
 
 directory "/etc/bind"
@@ -229,8 +229,8 @@ end
 
 zones[node[:dns][:domain]] = cluster_zone
 
-case node[:platform]
-when "redhat","centos"
+case node[:platform_family]
+when "rhel"
   template "/etc/sysconfig/named" do
     source "redhat-sysconfig-named.erb"
     mode 0644
@@ -247,8 +247,8 @@ when "suse"
 end
 
 service "bind9" do
-  case node[:platform]
-  when "centos","redhat","suse"
+  case node[:platform_family]
+  when "rhel", "suse"
     service_name "named"
   end
   supports restart: true, status: true, reload: true
